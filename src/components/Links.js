337c0d69db874575eba591436e1267ba7a7719c3
James Hallberg <Grebllah81788@gmail.com>
2023-09-25 16:51:54 -0400

    complete props basics lab

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,3 +1,13 @@
-function Links () {
-    return ()
+import React from 'react'
+
+function Links (props) {
+    return (
+       <div>
+            <h3>Links</h3>
+            <a href={props.github}>{props.github}</a>
+            <a href={props.linkedin}>{props.linkedin}</a>
+        </div>
+    );
 }
+
+export default Links
